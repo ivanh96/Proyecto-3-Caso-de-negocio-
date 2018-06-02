99f44d195ff6ddfaaba075de44ff683958c30f42
ivanh96 <ivanh96@hotmail.com>
2018-06-02 15:00:56 -0600

    Update proyecto.R

--- proyecto.R
+++ proyecto.R
@@ -1,13 +1,10 @@
 # install.packages("RPostgreSQL")
-# install.packages("random")
 # install.packages("randomNames")
 
-require("random")
 require("randomNames")
 require("RPostgreSQL")
 
 #setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-
 
 # CONNCECTION AND TABLES --------------------------------------------------
 {
@@ -23,7 +20,7 @@ require("RPostgreSQL")
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname = db, host = "localhost", port = 5432, user = us, password = ps)
 
-randoms<-read.csv("MOCK_DATA.csv", encoding = "UTF-8", check.names = F)
+randoms<-read.csv("MOCK_DATA2.csv", encoding = "UTF-8", check.names = F)
 
 
 {
@@ -39,60 +36,66 @@ for(d in 1:days){
   
   # NEW EMPLOYEE ------------------------------------------------------------
   Employee<-dbGetQuery(con, 'SELECT * FROM "Employee"')
-  EmployeeNew<-data.frame(matrix(ncol=length(Employee), nrow=0))
+  newEmployee<-vector()
   
   eId<-max(Employee$EmployeeId)
   
   i<-1
   n<-sample(1:5,1)
   while(i<=n){
+    EmployeeNew<-data.frame(matrix(ncol=length(Employee), nrow=0))
+    
     eId<-eId+1
     
     firstName<- randomNames(which.names = "first")
     lastName<- randomNames(which.names = "last")
     
-    title<- toString(randoms$title[sample(1:1000, 1)])
+    title<- toString(randoms$title[sample(1:500, 1)])
     rep<-sample(1:6, 1)
     
     birth<- as.Date(paste(sample(1947:1995,1), sample(1:12,1), sample(1:30,1), sep = "-"))
     hire<- as.Date(paste(sample(2002:2010,1), sample(1:12,1), sample(1:30,1), sep = "-")) 
     
-    address<- toString(randoms$address[sample(1:1000, 1)])
+    address<- toString(randoms$address[sample(1:500, 1)])
     
-    country<-"United States"#toString(randoms$country[sample(1:1000, 1)])
+    country<-"United States"#toString(randoms$country[sample(1:500, 1)])
     
     if(country=="United States"){
       state<- sample(state.abb, 1)
     }else{
       state<- ""
     }
-    city<- toString(randoms$city[sample(1:1000, 1)])
+    city<- toString(randoms$city[sample(1:500, 1)])
     postal<- toString(sample(state.area, 1))
-    phone<- toString(randoms$phone[sample(1:1000, 1)])
-    fax<- toString(randoms$phone[sample(1:1000, 1)])
-    email<-toString(randoms$email[sample(1:1000, 1)])
+    phone<- toString(randoms$phone[sample(1:500, 1)])
+    fax<- toString(randoms$phone[sample(1:500, 1)])
+    email<-toString(randoms$email[sample(1:500, 1)])
     
     x<-data.frame(eId, firstName, lastName, title, rep, birth, hire, address, city, state, country, postal, phone, fax, email)
     names(x)<-colnames(Employee)
     
     EmployeeNew <- rbind(x, EmployeeNew)
+    dbWriteTable(con, "Employee", value = EmployeeNew, append = TRUE, row.names = FALSE)
+    newEmployee[i]<-EmployeeNew$EmployeeId
     i<-i+1
   }
-  dbWriteTable(con, "Employee", value = EmployeeNew, append = TRUE, row.names = FALSE)
-  cat("SE AGREGARON",nrow(EmployeeNew),"FILAS A LA TABLA Employee\n")
+  cat("SE AGREGARON",length(newEmployee),"FILAS A LA TABLA Employee\n")
+  rm(Employee)
+  gc(verbose = FALSE)
   # /NEW EMPLOYEE ------------------------------------------------------------
   
   
   # NEW Artist ------------------------------------------------------------
   Artist<- dbGetQuery(con, 'SELECT * FROM "Artist"')
-  ArtistNew<-data.frame(matrix(ncol=length(Artist), nrow=0))
+  newArtist<-vector()
   
   aId<-max(Artist$ArtistId)
-  
   
   i<-1
   n<-sample(1:10,1)
   while(i<=n){
+    ArtistNew<-data.frame(matrix(ncol=length(Artist), nrow=0))
+    
     aId<-aId+1
     
     x<-sample(1:4, 1)
@@ -103,16 +106,16 @@ for(d in 1:days){
       name<-paste(firstName, lastName)
     }
     if(x==2){
-      firstName<- toString(randoms$phrase1[sample(1:1000, 1)])
-      lastName<- toString(randoms$phrase1[sample(1:1000, 1)])
+      firstName<- toString(randoms$phrase1[sample(1:500, 1)])
+      lastName<- toString(randoms$phrase1[sample(1:500, 1)])
       name<-paste(firstName, lastName)
     }
     if(x==3){
-      name<- toString(randoms$appname[sample(1:1000, 1)])
+      name<- toString(randoms$appname[sample(1:500, 1)])
     }
     if(x==4){
-      firstName<- toString(randoms$words1[sample(1:1000, 1)])
-      lastName<- toString(randoms$words2[sample(1:1000, 1)])
+      firstName<- toString(randoms$words1[sample(1:500, 1)])
+      lastName<- toString(randoms$words2[sample(1:500, 1)])
       name<-paste(firstName, lastName)
     }
     
@@ -121,42 +124,46 @@ for(d in 1:days){
     names(x)<-colnames(Artist)
     
     ArtistNew <- rbind(x, ArtistNew)
+    dbWriteTable(con, "Artist", value = ArtistNew, append = TRUE, row.names = FALSE)
+    newArtist[i]<-ArtistNew$ArtistId
     i<-i+1
   }
-  dbWriteTable(con, "Artist", value = ArtistNew, append = TRUE, row.names = FALSE)
-  cat("SE AGREGARON",nrow(ArtistNew),"FILAS A LA TABLA Artist\n")
+  cat("SE AGREGARON",length(newArtist),"FILAS A LA TABLA Artist\n")
+  rm(Artist)
+  gc(verbose = FALSE)
   # /NEW Atrtist ------------------------------------------------------------
   
   
   # NEW Album ------------------------------------------------------------
   Artist<- dbGetQuery(con, 'SELECT * FROM "Artist"')
   Album<- dbGetQuery(con, 'SELECT * FROM "Album"')
-  AlbumNew<-data.frame(matrix(ncol=length(Album), nrow=0))
+  newAlbum<-vector()
   
   bId<-max(Album$AlbumId)
-  
   
   i<-1
   n<-sample(1:10, 1)
   while(i<=n){
+    AlbumNew<-data.frame(matrix(ncol=length(Album), nrow=0))
+    
     bId<-bId+1
     
     x<-sample(1:4, 1)
     
     if(x==1){
-      title<- toString(randoms$phrase1[sample(1:1000, 1)])
+      title<- toString(randoms$phrase1[sample(1:500, 1)])
     }
     if(x==2){
-      firstName<- toString(randoms$phrase1[sample(1:1000, 1)])
-      lastName<- toString(randoms$phrase2[sample(1:1000, 1)])
+      firstName<- toString(randoms$phrase1[sample(1:500, 1)])
+      lastName<- toString(randoms$phrase2[sample(1:500, 1)])
       title<-paste(firstName, lastName)
     }
     if(x==3){
-      title<- toString(randoms$words1[sample(1:1000, 1)])
+      title<- toString(randoms$words1[sample(1:500, 1)])
     }
     if(x==4){
-      firstName<- toString(randoms$words1[sample(1:1000, 1)])
-      lastName<- toString(randoms$words2[sample(1:1000, 1)])
+      firstName<- toString(randoms$words1[sample(1:500, 1)])
+      lastName<- toString(randoms$words2[sample(1:500, 1)])
       title<-paste(firstName, lastName)
     }
     
@@ -164,47 +171,51 @@ for(d in 1:days){
     
     x<-data.frame(bId, title, aId)
     names(x)<-colnames(Album)
-    
     AlbumNew <- rbind(x, AlbumNew)
+    dbWriteTable(con, "Album", value = AlbumNew, append = TRUE, row.names = FALSE)
+    newAlbum[i]<-AlbumNew$AlbumId
     i<-i+1
   }
-  dbWriteTable(con, "Album", value = AlbumNew, append = TRUE, row.names = FALSE)
-  cat("SE AGREGARON",nrow(AlbumNew),"FILAS A LA TABLA Album\n")
+  cat("SE AGREGARON",length(newAlbum),"FILAS A LA TABLA Album\n")
+  rm(Artist)
+  rm(Album)
+  gc(verbose = FALSE)
   # /NEW Album ------------------------------------------------------------
   
   
   # NEW TRACK ------------------------------------------------------------
   Album<- dbGetQuery(con, 'SELECT * FROM "Album"')
   Track<- dbGetQuery(con, 'SELECT * FROM "Track"')
-  
-  TrackNew<-data.frame(matrix(ncol=length(Track), nrow=0))
+  newTrack<-vector()
+  k<-1
   
   tId<-max(Track$TrackId)
   
-  for(i in min(AlbumNew$AlbumId):max(AlbumNew$AlbumId)){
+  for(i in min(newAlbum):max(newAlbum)){
     
-    n<-sample(1:10, 1)
+    n<-sample(1:5, 1)
     j<-1
     while(n>=j){
+      TrackNew<-data.frame(matrix(ncol=length(Track), nrow=0))
       
       tId<-tId+1
       
       x<-sample(1:4, 1)
       
       if(x==1){
-        name<- toString(randoms$phrase1[sample(1:1000, 1)])
+        name<- toString(randoms$phrase1[sample(1:500, 1)])
       }
       if(x==2){
-        firstName<- toString(randoms$phrase1[sample(1:1000, 1)])
-        lastName<- toString(randoms$phrase2[sample(1:1000, 1)])
+        firstName<- toString(randoms$phrase1[sample(1:500, 1)])
+        lastName<- toString(randoms$phrase2[sample(1:500, 1)])
         name<-paste(firstName, lastName)
       }
       if(x==3){
-        name<- toString(randoms$words1[sample(1:1000, 1)])
+        name<- toString(randoms$words1[sample(1:500, 1)])
       }
       if(x==4){
-        firstName<- toString(randoms$words1[sample(1:1000, 1)])
-        lastName<- toString(randoms$words2[sample(1:1000, 1)])
+        firstName<- toString(randoms$words1[sample(1:500, 1)])
+        lastName<- toString(randoms$words2[sample(1:500, 1)])
         name<-paste(firstName, lastName)
       }
       
@@ -227,42 +238,51 @@ for(d in 1:days){
       x<-data.frame(tId, name, aId, type, genre, composer, ms, bytes, price)
       names(x)<-colnames(Track)
       TrackNew <- rbind(x, TrackNew)
+      dbWriteTable(con, "Track", value = TrackNew, append = TRUE, row.names = FALSE)
+      newTrack[k]<-TrackNew$TrackId
       j<-j+1
+      k<-k+1
     }
   }
-  dbWriteTable(con, "Track", value = TrackNew, append = TRUE, row.names = FALSE)
-  cat("SE AGREGARON",nrow(TrackNew),"FILAS A LA TABLA Track\n")
+  
+  cat("SE AGREGARON",length(newTrack),"FILAS A LA TABLA Track\n")
+  rm(Album)
+  rm(Track)
+  gc(verbose = FALSE)
   # /NEW TRACK ------------------------------------------------------------
   
   # NEW CUSTOMER ------------------------------------------------------------
   Customer<-dbGetQuery(con, 'SELECT * FROM "Customer"')
-  CustomerNew<-data.frame(matrix(ncol=length(Customer), nrow=0))
+  
+  newCustomer<-vector()
   
   cId<-max(Customer$CustomerId)
   
   i<-1
-  n<-sample(1:50, 1)
+  n<-sample(1:15, 1)
   while(i<=n){
+    CustomerNew<-data.frame(matrix(ncol=length(Customer), nrow=0))
+    
     cId<-cId+1
     
     firstName<- randomNames(which.names = "first")
     lastName<- randomNames(which.names = "last")
     
     company<- ""
-    address<- toString(randoms$address[sample(1:1000, 1)])
+    address<- toString(randoms$address[sample(1:500, 1)])
     
-    country<-toString(randoms$country[sample(1:1000, 1)])
+    country<-toString(randoms$country[sample(1:500, 1)])
     
     if(country=="United States"){
       state<- sample(state.abb, 1)
     }else{
       state<- ""
     }
-    city<- toString(randoms$city[sample(1:1000, 1)])
+    city<- toString(randoms$city[sample(1:500, 1)])
     postalCode<- toString(sample(state.area, 1))
-    phone<- toString(randoms$phone[sample(1:1000, 1)])
+    phone<- toString(randoms$phone[sample(1:500, 1)])
     fax<-""
-    email<-toString(randoms$email[sample(1:1000, 1)])
+    email<-toString(randoms$email[sample(1:500, 1)])
     
     repId<-unique(Customer$SupportRepId)
     support<-repId[sample(1:length(repId), 1)]
@@ -271,10 +291,14 @@ for(d in 1:days){
     names(x)<-colnames(Customer)
     
     CustomerNew <- rbind(x, CustomerNew)
+    dbWriteTable(con, "Customer", value = CustomerNew, append = TRUE, row.names = FALSE)
+    
+    newCustomer[i]<-CustomerNew$CustomerId
     i<-i+1
   }
-  dbWriteTable(con, "Customer", value = CustomerNew, append = TRUE, row.names = FALSE)
-  cat("SE AGREGARON",nrow(CustomerNew),"FILAS A LA TABLA Customeer\n")
+  
+  cat("SE AGREGARON",length(newCustomer),"FILAS A LA TABLA Customer\n")
+  gc(verbose = FALSE)
   # /NEW CUSTOMER ------------------------------------------------------------
   
   
@@ -288,7 +312,8 @@ for(d in 1:days){
   invoiceLineId<-max(InvoiceLine$InvoiceLineId)
   invoiceId<-max(Invoice$InvoiceId)
   
-  for(i in min(CustomerNew$CustomerId):max(CustomerNew$CustomerId)){
+  
+  for(i in min(newCustomer):max(newCustomer)){
     invoiceId<-invoiceId+1
     
     n<-sample(1:10, 1)
@@ -297,14 +322,14 @@ for(d in 1:days){
       invoiceLineId<-invoiceLineId+1
       
       track<-Track[sample(1:max(Track$TrackId), 1), ]
-      
-      
+      track
       x<-data.frame(invoiceLineId, invoiceId, track$TrackId, track$UnitPrice, 1)
       names(x)<-colnames(InvoiceLine)
       InvoiceLineNew <- rbind(x, InvoiceLineNew)
       j<-j+1
     }
   }
+  InvoiceLineNew<-na.omit(InvoiceLineNew)
   # /NEW INVOICELINE ------------------------------------------------------------
   
   
@@ -313,10 +338,12 @@ for(d in 1:days){
   
   invoiceId<-max(Invoice$InvoiceId)
   
-  for(i in min(CustomerNew$CustomerId):max(CustomerNew$CustomerId)){
+  for(i in min(newCustomer):max(newCustomer)){
+    customer<-dbGetQuery(con, 'SELECT * FROM "Customer"')
+    
     invoiceId<-invoiceId+1
     
-    customer<-CustomerNew[CustomerNew$CustomerId==i,]
+    customer<-Customer[customer$CustomerId==i,]
   
     invoiceDate<- as.Date(format(Sys.Date(), "%Y-%m-%d 00:00:00"))+d
     
@@ -331,9 +358,13 @@ for(d in 1:days){
   dbWriteTable(con, "InvoiceLine", value = InvoiceLineNew, append = TRUE, row.names = FALSE)
   cat("SE AGREGARON",nrow(InvoiceNew),"FILAS A LA TABLA Invoice\n")
   cat("SE AGREGARON",nrow(InvoiceLineNew),"FILAS A LA TABLA InvoiceLine\n")
-
+  rm(Invoice)
+  rm(InvoiceLine)
+  rm(Track)
+  gc(verbose = FALSE)
   # /NEW INVOICE ------------------------------------------------------------
   cat("DIA",d,"FINALIZADO\n")
+  
 }
 
 # close the connection
